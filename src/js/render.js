04338f7d3503e63cf7b89e0b8cd1d1cdfb4e3389
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-06-29 20:45:06 +0300

    validate  form

--- src/js/render.js
+++ src/js/render.js
@@ -110,18 +110,80 @@ function renderForm() {
   const chatContainer = document.getElementById("chatContainer");
   chatContainer.innerHTML += `
     <form class="chat__form" onsubmit="submitForm(event)">
-      <input class="chat__form__input" type="text" id="firstName" placeholder="Ім'я" required>
-      <input class="chat__form__input" type="text" id="lastName" placeholder="Прізвище" required>
-      <input class="chat__form__input" type="email" id="email" placeholder="Пошта" required>
+      <input class="chat__form__input" type="text" id="firstName" placeholder="Ім'я" required pattern=".{2,13}">
+    <div id="firstNameError" class="error-message"></div>
+<input class="chat__form__input" type="text" id="lastName" placeholder="Прізвище" required pattern=".{2,13}">
+<div id="lastNameError" class="error-message"></div>
+<input class="chat__form__input" type="email" id="email" placeholder="Пошта" required>
+<div id="emailError" class="error-message"></div>
       <input class="chat__form__input" type="tel" id="phone" placeholder="Телефон" pattern="[0-9]*" inputmode="numeric" required>
-      <button class="chat__form__input" type="submit">Надіслати</button>
+      <div id="phoneError" class="error-message"></div>
+      <button class="chat__form__button" type="submit">Надіслати</button>
     </form>`;
   scrollToBottom();
   const phoneInput = document.getElementById("phone");
   phoneInput.addEventListener("input", function () {
-    phoneInput.value = phoneInput.value.replace(/\D/g, "");
+    phoneInput.value = phoneInput.value.replace(/[^\d+]/g, "");
+  });
+
+  phoneInput.addEventListener("focus", function () {
+    if (!phoneInput.value.startsWith("+380")) {
+      phoneInput.value = "+380";
+    }
+  });
+  document.getElementById("firstName").addEventListener("input", function () {
+    const firstName = document.getElementById("firstName");
+    const firstNameError = document.getElementById("firstNameError");
+    if (firstName.value.length < 2) {
+      firstNameError.textContent = "Ім'я не може бути менше за 2 символів";
+    } else if (firstName.value.length > 13) {
+      firstNameError.textContent = "Ім'я не може бути більше 13 символів";
+    } else {
+      firstNameError.classList.add("message-true");
+      firstNameError.textContent = "Ваше ім'я Супер";
+    }
+  });
+
+  document.getElementById("lastName").addEventListener("input", function () {
+    const lastName = document.getElementById("lastName");
+    const lastNameError = document.getElementById("lastNameError");
+    if (lastName.value.length < 2) {
+      lastNameError.textContent = "Прізвище не може бути менше за 2 символів";
+    } else if (lastName.value.length > 13) {
+      lastNameError.textContent = "Прізвище не може бути більше 13 символів";
+    } else {
+      lastNameError.classList.add("message-true");
+      lastNameError.textContent = "Ваше прізвище Супер";
+    }
+  });
+  document.getElementById("email").addEventListener("input", function () {
+    const email = document.getElementById("email");
+    const emailError = document.getElementById("emailError");
+    if (!email.validity.valid) {
+      emailError.textContent = "Введіть коректну адресу електронної пошти";
+    } else {
+      emailError.classList.add("message-true");
+      emailError.textContent = "Почта ідеальна";
+    }
+  });
+
+  document.getElementById("phone").addEventListener("input", function () {
+    const phoneInput = document.getElementById("phone");
+    let trimmedValue = phoneInput.value.trim();
+    const phoneError = document.getElementById("phoneError");
+
+    if (!trimmedValue.startsWith("+380")) {
+      phoneError.textContent = "Номер телефону має починатися з +380";
+    } else if (trimmedValue.length !== 13) {
+      phoneError.textContent = "Номер телефону повинен складати 13 символів";
+    } else {
+      phoneInput.value = trimmedValue;
+      phoneError.classList.add("message-true");
+      phoneError.textContent = "Ваш номер телефону Супер";
+    }
   });
 }
+
 function scrollToBottom() {
   const chatContainer = document.getElementById("chatContainer");
   chatContainer.scrollTop = chatContainer.scrollHeight;
